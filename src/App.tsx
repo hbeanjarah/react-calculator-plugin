b399978fc55cee0ef9417601509260d841aa4014
hbeanjarah <hbeanjarahoussen@gmail.com>
2023-01-19 07:27:41 +0300

    feat: add calculation functionnality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calculator } from "./components/Calculator";
 
-const App = () => (
-  <div className="w-full flex justify-center">
-    <Calculator />
-  </div>
-);
+const App = () => {
+  const [calculatorState, setCalculatorState] = useState("");
+  return (
+    <div className="w-full flex justify-center">
+      <Calculator
+        calculatorState={calculatorState}
+        setCalculatorState={setCalculatorState}
+      />
+    </div>
+  );
+};
 
 export default App;
