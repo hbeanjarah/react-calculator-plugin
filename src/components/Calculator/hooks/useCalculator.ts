ccf1de31d9313b7b9ccfa7e88364fba5e559c9a1
hbeanjarah <hbeanjarahoussen@gmail.com>
2023-01-19 22:34:25 +0300

    feat: add keyboard support for numeric input

--- src/components/Calculator/hooks/useCalculator.ts
+++ src/components/Calculator/hooks/useCalculator.ts
@@ -1,17 +1,55 @@
 import React from "react";
 
-export const useCalculator = () => {
+export const useCalculator = ({
+  setCalculatorState,
+}: {
+  setCalculatorState: (value: React.SetStateAction<string>) => void;
+}) => {
   const validateInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    const keyCode = +event.key;
-    const excludedKeys = [8, 37, 39, 46];
-    if (
-      !(
-        (keyCode >= 48 && keyCode <= 57) ||
-        (keyCode >= 96 && keyCode <= 105) ||
-        excludedKeys.includes(keyCode)
-      )
-    ) {
+    let authorizedKeyCode = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      ".",
+      "Divide",
+      "Comma",
+      "Multiply",
+      "Enter",
+      "Subtract",
+      "Add",
+      "Period",
+    ];
+    const isNumpad = event.code.includes("Numpad");
+    const isDigit = event.code.includes("Digit");
+
+    let splittedKeyCode;
+
+    if (isNumpad) {
+      splittedKeyCode = event.code.split("Numpad");
+    } else if (isDigit) {
+      splittedKeyCode = event.code.split("Digit");
+    } else {
+      splittedKeyCode = "";
+    }
+
+    if (splittedKeyCode === "" && authorizedKeyCode.includes(event.code)) {
+      setCalculatorState((prev) => prev + event.key);
+    } else if (splittedKeyCode !== "" && splittedKeyCode.length > 0) {
+      const currentKeyCode = splittedKeyCode[1];
+      authorizedKeyCode.includes(currentKeyCode) &&
+        setCalculatorState((prev) => prev + event.key);
+    } else {
       event.preventDefault();
+    }
+    if (event.code === "Backspace") {
+      setCalculatorState((prev) => prev.slice(0, -1));
     }
   };
 
